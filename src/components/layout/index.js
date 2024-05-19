a02cdbf5a37dafadd681f1db72e94d0960d4d981
jenish-jain <jenishjain@rocketmail.com>
2024-05-19 12:52:17 +0530

    adding GA to website

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,9 @@
-import { Header } from '../header';
-import { Main } from '../main';
-import { Footer } from '../footer/index';
-import { useSettings } from '../../context/settings';
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
+import { Header } from "../header";
+import { Main } from "../main";
+import { Footer } from "../footer/index";
+import { useSettings } from "../../context/settings";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_TRACKING_ID);
 const Layout = ({ children }) => {
@@ -14,8 +14,17 @@ const Layout = ({ children }) => {
   }, []);
 
   return (
-    <div className={`outer ${darkMode ? 'dark' : 'light'}`}>
+    <div className={`outer ${darkMode ? "dark" : "light"}`}>
       <Header />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-M9D0F8MKP6"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-M9D0F8MKP6');
+      </script>
       <Main>{children}</Main>
       <Footer />
     </div>
